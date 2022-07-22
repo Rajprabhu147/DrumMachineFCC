6c7db92730c4765e81abf58c943cc96ab08766bf
KingLord147 <rajprabhu.147@gmail.com>
2022-07-22 18:41:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,104 @@
+const sounds = [
+    {
+      key: 'Q',
+      mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+    },
+    {
+      key: 'W',
+      mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+    },
+    {
+      key: 'E',
+      mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
+    },
+    {
+      key: 'A',
+      mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
+    },
+    {
+      key: 'S',
+      mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
+    },
+    {
+      key: 'D',
+      mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+    },
+    {
+      key: 'Z',
+      mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+    },
+    {
+      key: 'X',
+      mp3: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+    },
+    {
+      key: 'C',
+      mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+    },
+  ];
+  
+  const App = () => (
+    <div id="display" className="display">
+    <title>Drum Machine</title>
+      <h1>Click or Press Key to Play a sound</h1>
+      {sounds.map((sound, idx) => (
+        <DrumPad text={sound.key} key={idx} audio={sound.mp3} />
+      ))}
+      <p className="footer">By Rajprabhu Rajasekaran</p>
+    </div>
+  );
+  
+  class DrumPad extends React.Component {
+    constructor(props) {
+      super(props);
+      
+      this.audio = React.createRef();
+    }
+    
+    componentDidMount() {
+      this.audio.current.addEventListener('ended', (e) => {
+        const parent = e.target.parentNode;
+        parent.classList.remove('active');
+      });
+    }
+    
+    playSound = () => {
+      this.audio.current.play();
+      
+      const id = this.audio.current.id;
+      
+      const parent = this.audio.current.parentNode;
+      parent.classList.add('active');
+      
+      const display = parent.parentNode;
+      display.querySelector('h1').innerText = `${id} is playing`;
+    }
+    
+    render() {
+      const { text, audio } = this.props;
+      
+      return (
+        <div className="drum-pad" onClick={this.playSound} id={`drum-${text}`}>
+          {text}
+          <audio ref={this.audio} src={audio} className="clip" id={text} />
+        </div>
+      )
+    }
+  }
+      
+  document.addEventListener('keydown', (e) => {
+    const id = e.key.toUpperCase();
+    const audio = document.getElementById(id);
+    
+    if(audio) {
+      audio.currentTime = 0;
+      const parent = audio.parentNode;
+      parent.classList.add('active');
+      
+      const display = parent.parentNode;
+      display.querySelector('h1').innerText = `${id} is playing`;
+      audio.play();
+    }
+  });
+  
+  ReactDOM.render(<App />, document.getElementById('drum-machine'));
